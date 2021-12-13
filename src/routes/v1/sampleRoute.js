a5b4f6db901e7b60333873d574eff9f73c513a42
z3b0w <andryslovas@gmail.com>
2021-12-13 09:06:37 +0200

    routes

--- src/routes/v1/sampleRoute.js
+++ src/routes/v1/sampleRoute.js
@@ -6,4 +6,14 @@ router.get('/', async (req, res) => {
   res.json('sample route');
 });
 
+router.post('/api/auth/login', async (req, res) => {
+  res.json('login');
+  console.log('login success', req.body);
+});
+
+router.post('/api/auth/register', async (req, res) => {
+  res.json('register');
+  console.log('register success', req.body);
+});
+
 module.exports = router;
